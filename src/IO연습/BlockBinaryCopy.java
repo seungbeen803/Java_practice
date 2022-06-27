fdad8ccc75801820be6ab898affa60ffeccff07b
seungbeen <seungbin803@gmail.com>
2022-06-27 23:54:45 +0900

    Java 에제 7

--- src/IO연습/BlockBinaryCopy.java
+++ src/IO연습/BlockBinaryCopy.java
@@ -1,20 +1,24 @@
-package IO;
+package IO연습;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class BinaryCopyEx {
+public class BlockBinaryCopy {
     public static void main(String[] args) {
-        File src = new File("c:\\Windows\\Web\\Wallpaper\\Theme2\\img8.jpg"); // 윈도우 10의 경우
-        File dest= new File("copyimg.jpg");
-        int c;
+        File src = new File("c:\\Windows\\Web\\Wallpaper\\Theme2\\img10.jpg");
+        File dest = new File("spring.jpg");
         try{
             FileInputStream fi = new FileInputStream(src);
             FileOutputStream fo = new FileOutputStream(dest);
-            while ((c = fi.read()) != -1){
-                fo.write((byte)c);
+
+            byte []buf = new byte[1024*10];
+            while(true){
+                int n = fi.read(buf);
+                fo.write(buf, 0, 2);
+                if(n < buf.length)
+                    break;
             }
             fi.close();
             fo.close();
