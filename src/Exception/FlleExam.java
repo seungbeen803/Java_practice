5a59c7e906cf4ff6588054ace986fd37e993db7d
seungbeen <seungbin803@gmail.com>
2022-09-24 23:53:09 +0900

    9월 24일 중간고사 자바 예외처리 교과서 예제연습

--- src/Exception/FlleExam.java
+++ src/Exception/FlleExam.java
@@ -1,4 +1,19 @@
 package Exception;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class FlleExam {
+    public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader("datar.txt");
+            FileWriter fw = new FileWriter("datar.txt");
+            int c;
+            while((c = fr.read()) != -1) {
+                fw.write(c);
+            }
+        }catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 }
