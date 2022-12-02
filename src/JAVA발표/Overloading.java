2a9145511b7bff14321db576979363388f5ac682
seungbeen <seungbin803@gmail.com>
2022-12-03 00:41:45 +0900

    12월 3일 java 발표 예제 작성

---
+++ src/JAVA발표/Overloading.java
@@ -0,0 +1,25 @@
+package JAVA발표;
+
+class Add {
+    // plus() 메서드 생성
+    // 매개 변수가 각각 int, double 타입으로 선언된 plus() 메서드 생성
+    int plus(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+
+    double plus(double x, double y) {
+        double result = x + y;
+        return result;
+    }
+}
+
+public class Overloading {
+    public static void main(String[] args) {
+        Add sum = new Add();
+        // 오버로딩된 메서드를 호출할 경우 JVM은 매개값의 타입을 보고 메서드를 선택합니다.
+        System.out.println(sum.plus(10, 20)); // int 타입의 plus() 메서드 실행
+        System.out.println(sum.plus(10.0, 20.0)); // double 타입의 plus() 메서드 실행
+    }
+}
+
